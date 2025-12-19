13e1c03ff4760bfc6654d00eeb1e2312e8cd1aac
Vipul Tyagi <88539287+iamvipultyagi@users.noreply.github.com>
2025-12-19 11:03:44 +0530

    Delete config-overrides.js

--- config-overrides.js
+++
@@ -1,8 +0,0 @@
-const Dotenv = require('dotenv-webpack');
-
-module.exports = function override(config, env) {
-  // Add dotenv-webpack plugin
-  config.plugins.push(new Dotenv());
-  
-  return config;
-};
